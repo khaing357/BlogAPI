3e53e845508b33843f1219da13179a831df92ab3
Khaing <khaingnyothein357@gmail.com>
2023-05-29 05:09:54 +0630

    edit routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Helpers\ResponseHelper;
@@ -8,22 +7,21 @@ use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
 
-
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
 
 Route::middleware(['auth:api'])->group(function(){
-    Route::post('logout',[AuthController::class,'logout']);
+Route::post('logout',[AuthController::class,'logout']);
 
-    //Profile
-    Route::get('profile',[ProfileController::class,'profile']);
-    Route::get('profilePosts',[ProfileController::class,'index']);
+//Profile
+Route::get('profile',[ProfileController::class,'profile']);
+Route::get('profilePosts',[ProfileController::class,'index']);
 
-    //Category
-    Route::get('categories',[CategoryController::class,'index']);
+ //Category
+ Route::get('categories',[CategoryController::class,'index']);
 
-    //Post
-    Route::get('posts',[PostController::class,'index']);
-    Route::get('posts/{postId}',[PostController::class,'show']);
-    Route::post('post',[PostController::class,'create']);
+ //Post
+ Route::get('posts',[PostController::class,'index']);
+ Route::get('posts/{postId}',[PostController::class,'show']);
+ Route::post('post',[PostController::class,'create']);
 });
